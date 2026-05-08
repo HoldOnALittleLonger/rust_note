8129e8d7ecc8a3bcddf124dfd06b8b0e418dbd1e
Zephyr <emailunspecified@false.com>
2026-05-08 16:03:14 +0800

    31

---
+++ multiple_thread_httpd/src/lib.rs
@@ -0,0 +1,136 @@
+use std::{
+    vec,
+    thread,
+    sync::{Arc, Mutex, mpsc},
+};
+
+/*
+ * Job - type alias to Smart Pointer of trait object,
+ *       the trait object is a callable object and
+ *       can be sent between multi-threads,lifetime
+ *       is entirely.
+ *       actually,a job is a callable object which will
+ *       deal with something.
+ */
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+/*
+ * ThreadPool - thread pool to holds workers and a communication
+ *              channel to workers
+ * @workers:    all workers
+ * @request_sender:
+ *              mpsc::Sender<> type object used to send Job to
+ *              worker
+ */
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    request_sender: mpsc::Sender<Job>,
+}
+
+/*
+ * Worker - worker descriptor used to represents work thread
+ * @id:     identifier
+ * @local_thread:
+ *          thead handle for this worker
+ * # this type is private to this crate,used by ThreadPool type.
+ */
+struct Worker {
+    id: usize,
+    local_thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    /// Constructor for Worker type.
+    /// @id: identifier of this worker.
+    /// @arc_rx:
+    ///      Atomic Reference Count type wrapped a Mutex object,
+    ///      in which a mpsc::Receiver is contained.
+    ///      this parameter is cloned from other context,thus it is
+    ///      safe to be moved into new thread.
+    fn new(id: usize, arc_rx: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        println!("start worker {id}");
+        Self {
+            id,
+            local_thread: thread::spawn(
+                move || {
+                    loop {
+                        let job = arc_rx.lock().expect("should got mutex lock.")
+                            .recv().unwrap();
+                        /* println!() always takes reference than ownership. */
+                        println!("worker {id} got a job.");
+                        job();
+                    }
+
+                    /*
+                     *
+                     *  MutexGuard will release this mutex at the timepoint
+                     *  after once iteration ends but before next iteration
+                     *  starts,thus the behavior would not be expected by us.
+                     *
+                    while let Ok(job) = 
+                        arc_rx.lock().expect("should got mutex lock.").recv() {
+
+                        println!("worker {id} got a job.");
+                        job();
+
+                    }
+                    */
+
+                    }
+                ),
+            }
+    }
+}
+
+/*
+ * ThreadPoolError - enumeration type to enumerates error status of
+ *                   ThreadPool object.
+ */
+#[derive(Debug)]
+pub enum ThreadPoolError {
+    TP_ERROR_BAD_AVAILABLE,
+}
+
+impl ThreadPool {
+
+    /// Constructor of ThreadPool type.
+    /// @available: specify the maximum number of workers in the pool,
+    ///             can not be _zero_.
+    pub fn new(available: usize) -> Result<Self, ThreadPoolError> {
+        if available == 0 {
+            Err(ThreadPoolError::TP_ERROR_BAD_AVAILABLE)
+        } else {
+            let mut workers_vec: Vec<Worker>
+                = Vec::with_capacity(available);
+
+            let (tx, rx) = mpsc::channel();
+
+            /* move @rx */
+            let arc_rx = Arc::new(Mutex::new(rx));
+
+            /* initialize workers */
+            for id in 0..available {
+                workers_vec.push(Worker::new(id, Arc::clone(&arc_rx)));
+            }
+
+            Ok(
+                Self {
+                    workers: workers_vec,
+                    /* move @tx */
+                    request_sender: tx,
+                }
+            )
+        }
+    }
+
+    /// Member method execute() used to send Job to worker.
+    /// @f: callable object and is not re-entrant,can be
+    ///     sent between multi-threads.
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static
+    {
+        let new_job: Job = Box::new(f);
+        self.request_sender.send(new_job).unwrap();
+    }
+}
---
+++ multiple_thread_httpd/src/main.rs
@@ -0,0 +1,74 @@
+/* This program just the example from The Book.
+ * All error handling just simply panics.
+ */
+
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{BufReader, prelude::*},
+    thread,
+    time::Duration,
+    fs,
+    string,
+    vec
+};
+
+/* crate alias */
+use multiple_thread_httpd as mthttpd;
+use mthttpd::*;
+
+/*
+ * handle_http - function to handles http request
+ * @connection:  tcp stream for current connection
+ */
+fn handle_http(mut connection: TcpStream) {
+    let buffer: BufReader<_> = BufReader::new(&connection);
+    let http_request: Vec<_> = buffer
+        .lines()
+        .map(|iter_line| iter_line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    if http_request.len() == 0 {
+        println!("received invalid request,ignore.");
+        return;
+    }
+
+    println!("received http request:");
+    println!("{http_request:#?}");
+
+    const OK_ACCESS: &str = "hello.html";
+    const BAD_ACCESS: &str = "404.html";
+
+    let (status_line, filename) =
+        match &(http_request[0])[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", OK_ACCESS),
+            "GET /sleep HTTP/1.1" => {
+                thread::sleep(Duration::from_secs(10));
+                ("HTTP/1.1 200 OK", OK_ACCESS)
+            },
+            _ => ("HTTP/1.1 404 NOT FOUND", BAD_ACCESS),
+        };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let contents_length = contents.len();
+    let http_respone = 
+        format!("{status_line}\r\nContent-Length: \
+                 {contents_length}\r\n\r\n{contents}");        
+    /* '\' is used to tell rust do not include newline in the string */
+    connection.write_all(http_respone.as_bytes()).unwrap();
+}
+
+fn main() {
+    const IPv4_ENDPOINT: &str = "127.0.0.1:8080";
+    println!("httpd starting at {} .", IPv4_ENDPOINT);
+
+    let thread_pool: ThreadPool = ThreadPool::new(4).unwrap();
+
+    let tcp_listener = TcpListener::bind(IPv4_ENDPOINT).unwrap();
+    println!("httpd started.");
+
+    for iter_connection in tcp_listener.incoming() {
+        let connection = iter_connection.unwrap();
+        thread_pool.execute(|| handle_http(connection));
+    }
+}
